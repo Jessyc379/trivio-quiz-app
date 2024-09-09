7547bb3c71a714acdf3bc8fe4ec5d8183660f100
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-09-08 22:13:00 -0700

    update AnswerController - edit answer

--- src/main/java/com/nianti/controllers/AnswerController.java
+++ src/main/java/com/nianti/controllers/AnswerController.java
@@ -33,13 +33,14 @@ public class AnswerController {
         return "/answers/answers";
     }
 
-//    @GetMapping("/questions/{quizId}/add")
+    //    @GetMapping("/questions/{quizId}/add")
 //    public String addQuestion(Model model, @PathVariable int quizId) {
 //        Question question = new Question();
 //        question.setQuizId(quizId);
 //
 //        model.addAttribute("question", question);
 //        model.addAttribute("action", "add");
+//        model.addAttribute("title", "Add Answer");
 //
 //        return "/questions/add-edit";
 //    }
@@ -60,31 +61,32 @@ public class AnswerController {
 //
 //    }
 //
-//    @GetMapping("/questions/{quizId}/{questionId}/edit")
-//    public String editQuestion(Model model, @PathVariable int quizId, @PathVariable int questionId) {
-//        Question question = questionDao.getQuestionByQuestionId(questionId);
-//
-//        model.addAttribute("question", question);
-//        model.addAttribute("action", "edit");
-//
-//        return "/questions/add-edit";
-//    }
-//
-//    @PostMapping("/questions/{quizId}/{questionId}/edit")
-//    public String editQuestion(Model model,
-//                               @Valid @ModelAttribute("question") Question question, BindingResult result,
-//                               @PathVariable int quizId,
-//                               @PathVariable int questionId) {
-//        if (result.hasErrors()) {
-//            model.addAttribute("isInvalid", true);
-//            model.addAttribute("action", "edit");
-//            return "/questions/add-edit";
-//        }
-//        question.setQuestionId(questionId);
-//        question.setQuizId(quizId);
-//        questionDao.updateQuestion(question);
-//
-//        return "redirect:/questions?quizId=" + question.getQuizId();
-//
-//    }
+    @GetMapping("/answers/{questionId}/{answerId}/edit")
+    public String editAnswer(Model model, @PathVariable int answerId) {
+        Answer answer = answerDao.getAnswerByAnswerId(answerId);
+
+        model.addAttribute("answer", answer);
+        model.addAttribute("action", "edit");
+        model.addAttribute("title", "Edit Answer");
+
+        return "/answers/add-edit";
+    }
+
+    @GetMapping("/answers/{questionId}/{answerId}/edit")
+    public String editAnswer(Model model,
+                             @Valid @ModelAttribute("answer") Answer answer, BindingResult result,
+                             @PathVariable int questionId,
+                             @PathVariable int answerId) {
+        if (result.hasErrors()) {
+            model.addAttribute("isInvalid", true);
+            model.addAttribute("action", "edit");
+            return "/answers/add-edit";
+        }
+        answer.setAnswerId(answerId);
+        answer.setQuestionId(questionId);
+        answerDao.updateAnswer(answer);
+
+        return "redirect:/answers?questionId=" + questionId;
+
+    }
 }
